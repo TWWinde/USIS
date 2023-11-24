ddb1cdf62a78fe168daa483583701357924b2315
TWWinde <twwinde@gmail.com>
2023-11-24 04:04:45 +0100

    Create batch.sh

---
+++ batch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+
+#Slurm parameters
+#SBATCH --job-name=medical
+#SBATCH --output=medical%j.%N.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=6-23:00:00
+#SBATCH --mem=64G
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --qos=batch
+# SBATCH --nodes=1
+# SBATCH --gpus=rtx_a5000:1
+# SBATCH --gpus=geforce_rtx_2080ti:1
+# SBATCH --gpus=geforce_gtx_titan_x:1
+
+# Activate everything you need
+#echo $PYENV_ROOT
+#echo $PATH
+pyenv activate moose_env
+module load cuda
+# module load cuda/11.3
+
+
+
+CUDA_VISIBLE_DEVICES=0 python /misc/no_backups/s1449/USIS/dataloaders/data_preparation.py
