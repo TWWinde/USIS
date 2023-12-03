94651fd17e1f0aed923931ff6ef6098eb466a2f7
TWWinde <twwinde@gmail.com>
2023-12-03 10:09:29 +0100

    Update TotalSegmentor.py

--- dataloaders/TotalSegmentor.py
+++ dataloaders/TotalSegmentor.py
@@ -1,6 +1,14 @@
 from totalsegmentator.python_api import totalsegmentator
-
+import os
 if __name__ == "__main__":
-    input_path = '/data/private/autoPET/Task1/pelvis/1PA001/ct.nii.gz'
+    root_path = '/data/private/autoPET/Task1/pelvis/'
     output_path = '/data/private/autoPET/Task1/ct_label'
-    totalsegmentator(input_path, output_path)
+    people_name = os.listdir(root_path)
+    for item in people_name:
+        if item != 'overview':
+            input_path = os.path.join(root_path, item, 'ct.nii.gz')
+            output_path = os.path.join(output_path, item)
+            os.makedirs(output_path, exist_ok=True)
+            totalsegmentator(input_path, output_path)
+
+    
