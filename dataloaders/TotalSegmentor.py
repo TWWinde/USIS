256bba2cd2494085c8bd6dcb1f998a5dfe9bc76b
TWWinde <twwinde@gmail.com>
2023-12-07 09:40:59 +0100

    Update TotalSegmentor.py

--- dataloaders/TotalSegmentor.py
+++ dataloaders/TotalSegmentor.py
@@ -140,8 +140,8 @@ def total_segmentor(root_path, output_root_path):
             input_path = os.path.join(root_path, item, 'ct.nii.gz')
             output_path = os.path.join(output_root_path, item)
             os.makedirs(output_path, exist_ok=True)
-            #totalsegmentator(input_path, output_path, task='body')
-            totalsegmentator(input_path, output_path, task='tissue_types')
+            totalsegmentator(input_path, output_path, task='body')
+            #totalsegmentator(input_path, output_path, task='tissue_types')
 
 
 if __name__ == "__main__":
