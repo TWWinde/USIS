1380ec97dcfba2d859d4352582ed61148dd4a705
TWWinde <twwinde@gmail.com>
2023-12-07 16:27:28 +0100

    Update losses.py

--- models/losses.py
+++ models/losses.py
@@ -50,6 +50,8 @@ def get_n1_target(opt, input, label, target_is_real):
     num_of_classes = label.shape[1]
     integers = torch.argmax(label, dim=1)
     targets = targets[:, 0, :, :] * num_of_classes
+    print('targets.shape', targets.shape)
+    print('integers.shape', integers.shape)
     integers += targets.long()
     integers = torch.clamp(integers, min=num_of_classes-1) - num_of_classes + 1
     return integers
