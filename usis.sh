6979b91e9ab3fd1eeb81901e75fc17032b7d3be5
TWWinde <twwinde@gmail.com>
2023-12-09 16:05:51 +0100

    Update usis.sh

--- usis.sh
+++ usis.sh
@@ -24,7 +24,7 @@ module load cuda
 
 python train.py --name usis_wavelet --dataset_mode ct2mri --gpu_ids 0 \
 --dataroot /misc/data/private/autoPET/data_nnunet --batch_size 2 --add_mask  \
---netDu wavelet \
+--netDu wavelet --continue_train\
 --model_supervision 0 --netG wavelet --channels_G 16
 
 #python train.py --name usis --dataset_mode cityscapes --gpu_ids 0 \
