6204452c610588e87f5996a263d02a28cd7c84b6
TWWinde <twwinde@gmail.com>
2023-12-15 10:27:54 +0100

    Update TotalSegmentator_combine_masks.py

--- dataloaders/TotalSegmentator_combine_masks.py
+++ dataloaders/TotalSegmentator_combine_masks.py
@@ -45,8 +45,7 @@ def main():
         subprocess.call(f"/opt/nora/src/node/nora -p {args.nora_tag} --add {args.output} --addtag mask", shell=True)
 
 
-masks = ["lung", "lung_left", "lung_right",
-         "vertebrae", "ribs", "vertebrae_ribs", "heart", "pelvis", "body"]
+masks = ["lung", "vertebrae", "ribs", "vertebrae_ribs", "heart", "pelvis", "body", 'kidney', 'kidney', 'adrenal', 'gluteus']
 
 
 def masks_combine(path):
