a50c297db25d46081ce4c408a7bc80f439136065
TWWinde <twwinde@gmail.com>
2023-12-16 12:27:13 +0100

    Update usis.sh

--- usis.sh
+++ usis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 #Slurm parameters
-#SBATCH --job-name=med_usis2
+#SBATCH --job-name=med_usis
 #SBATCH --output=med_usis%j.%N.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
@@ -23,10 +23,10 @@ module load cuda
 # Run your python code
 
 #experiment_1
-python train.py --name usis_wavelet --dataset_mode ct2mri --gpu_ids 0 \
---dataroot /misc/data/private/autoPET/CT_MR --batch_size 2 --add_mask  \
---netDu wavelet \
---model_supervision 0 --netG wavelet --channels_G 16  #16
+#python train.py --name usis_wavelet --dataset_mode ct2mri --gpu_ids 0 \
+#--dataroot /misc/data/private/autoPET/CT_MR --batch_size 2 --add_mask  \
+#--netDu wavelet \
+#--model_supervision 0 --netG wavelet --channels_G 16  #16
 
 #experiment_2
 #python train.py --name usis_wavelet_no_mask --dataset_mode ct2mri --gpu_ids 0 \
@@ -36,10 +36,10 @@ python train.py --name usis_wavelet --dataset_mode ct2mri --gpu_ids 0 \
 
 
 #experiment_2
-#python train.py --name usis_oasis_generator --dataset_mode ct2mri --gpu_ids 0 \
-#--dataroot /misc/data/private/autoPET/CT_MR --batch_size 4 --add_mask  \
-#--netDu wavelet --continue_train \
-#--model_supervision 0 --netG oasis --channels_G 64
+python train.py --name usis_oasis_generator --dataset_mode ct2mri --gpu_ids 0 \
+--dataroot /misc/data/private/autoPET/CT_MR --batch_size 2 --add_mask  \
+--netDu wavelet --continue_train \
+--model_supervision 0 --netG oasis --channels_G 64
 
 
 #python test.py --name oasis_cityscapes_wavelet_disc --dataset_mode cityscapes --gpu_ids 0 \
