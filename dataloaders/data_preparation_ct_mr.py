4ed1ac9dbbdbf468b27f389382a07e8f75eb90fe
TWWinde <twwinde@gmail.com>
2023-12-16 11:23:39 +0100

    Update data_preparation_ct_mr.py

--- dataloaders/data_preparation_ct_mr.py
+++ dataloaders/data_preparation_ct_mr.py
@@ -43,7 +43,7 @@ def get_2d_mr_images(mr_path, ct_path, ct_label_path):
 
                 height, width = ct_label_slice.shape
                 target_size = max(height, width)
-                square_image = np.zeros((target_size, target_size, 3), dtype=np.uint8)
+                square_image = np.zeros((target_size, target_size), dtype=np.uint8)
                 x_offset = (target_size - width) // 2
                 y_offset = (target_size - height) // 2
                 square_image[y_offset:y_offset + height, x_offset:x_offset + width] = ct_label_slice
