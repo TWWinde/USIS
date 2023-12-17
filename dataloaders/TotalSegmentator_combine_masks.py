96fb23960d4af4d51dbc735b4eb9672e7eb29d9f
TWWinde <twwinde@gmail.com>
2023-12-17 17:21:33 +0100

    update

--- dataloaders/TotalSegmentator_combine_masks.py
+++ dataloaders/TotalSegmentator_combine_masks.py
@@ -1,3 +1,4 @@
+import shutil
 import sys
 from pathlib import Path
 import argparse
@@ -8,44 +9,15 @@ import numpy as np
 from totalsegmentator.libs import combine_masks
 
 
-def main():
-    """
-    Combine binary labels into a binary file.
-
-    Works with any number of label files.
-
-    Usage:
-    totalseg_combine_masks -i totalsegmentator_output_dir -o combined_mask.nii.gz -m lung
-    """
-    parser = argparse.ArgumentParser(description="Combine masks.",
-                                     epilog="Written by Jakob Wasserthal. If you use this tool please cite https://pubs.rsna.org/doi/10.1148/ryai.230024")
-
-    parser.add_argument("-i", metavar="directory", dest="mask_dir",
-                        help="TotalSegmentator output directory containing all the masks",
-                        type=lambda p: Path(p).absolute(), required=True)
-
-    parser.add_argument("-o", metavar="filepath", dest="output",
-                        help="Output path for combined mask",
-                        type=lambda p: Path(p).absolute(), required=True)
-
-    parser.add_argument("-m", "--masks", type=str, choices=["lung", "lung_left", "lung_right",
-                                                            "vertebrae", "ribs", "vertebrae_ribs", "heart", "pelvis",
-                                                            "body"],
-                        help="The type of masks you want to combine", required=True)
-
-    parser.add_argument("-t", "--nora_tag", type=str, help="tag in nora as mask. Pass nora project id as argument.",
-                        default="None")
-
-    args = parser.parse_args()
-
-    combined_img = combine_masks(args.mask_dir, args.masks)
-    nib.save(combined_img, args.output)
-
-    if args.nora_tag != "None":
-        subprocess.call(f"/opt/nora/src/node/nora -p {args.nora_tag} --add {args.output} --addtag mask", shell=True)
-
 
 masks = ["humerus"]
+
+
+def move_images(path1, path2, num):
+    for i in range(num):
+        file_path = os.path.join(path1, f'slice_{i}.png')
+        shutil.copyfile(file_path, os.path.join(path2, f'slice_{i}.png'))
+
 
 
 def masks_combine(path):
@@ -62,4 +34,7 @@ def masks_combine(path):
 
 if __name__ == "__main__":
     root_path = '/data/private/autoPET/Task1/ct_label'
-    masks_combine(root_path)
+    path1 = '/misc/data/private/autoPET/CT_MR/mr/train'
+    path2 = '/misc/data/private/autoPET/CT_MR/mr/val'
+    move_images(path1, path2, 500)
+    #masks_combine(root_path)
---
+++ util/metrics.py
@@ -0,0 +1,133 @@
+import os
+import numpy as np
+import torch
+import time
+from scipy import linalg # For numpy FID
+from pathlib import Path
+from PIL import Image
+import models.models as models
+import matplotlib.pyplot as plt
+import torchvision.models
+import cv2
+import torch
+import torchvision.transforms as transforms
+import pytorch_msssim
+from pytorch_msssim import ssim
+import lpips
+import torch
+from skimage.metrics import peak_signal_noise_ratio, mean_squared_error
+# --------------------------------------------------------------------------#
+# This code is to calculate and save SSIM PIPS PSNR RMSE
+# --------------------------------------------------------------------------#
+
+
+
+class metrics():
+    def __init__(self, opt, dataloader_val):
+        self.opt = opt
+        self.val_dataloader = dataloader_val
+        self.path_to_save = os.path.join(self.opt.checkpoints_dir, self.opt.name)
+        self.path_to_save_PIPS = os.path.join(self.opt.checkpoints_dir, self.opt.name, "PIPS")
+        self.path_to_save_SSIM = os.path.join(self.opt.checkpoints_dir, self.opt.name, "SSIM")
+        self.path_to_save_PSNR = os.path.join(self.opt.checkpoints_dir, self.opt.name, "PSNR")
+        self.path_to_save_RMSE = os.path.join(self.opt.checkpoints_dir, self.opt.name, "RMSE")
+
+        Path(self.path_to_save_SSIM).mkdir(parents=True, exist_ok=True)
+        Path(self.path_to_save_PIPS).mkdir(parents=True, exist_ok=True)
+        Path(self.path_to_save_PSNR).mkdir(parents=True, exist_ok=True)
+        Path(self.path_to_save_RMSE).mkdir(parents=True, exist_ok=True)
+
+    def compute_metrics(self, netG, netEMA, model=None):
+        pips, ssim, psnr, rmse  = [], [], [], []
+        loss_fn_alex = lpips.LPIPS(net='alex')
+        netG.eval()
+        transform1 = transforms.Compose([
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # normalized to [0, 1]
+        ])
+        transform2 = transforms.Compose([
+            transforms.Normalize(mean=[0, 0, 0], std=[0.5, 0.5, 0.5])  # normalized to [-1, 1]
+        ])
+        if not self.opt.no_EMA:
+            netEMA.eval()
+
+        total_samples = len(self.val_dataloader)
+        with torch.no_grad():
+            for i, data_i in enumerate(self.val_dataloader):
+                image, label = models.preprocess_input(self.opt, data_i)
+                edges = model.module.compute_edges(image)
+                if self.opt.no_EMA:
+                    generated = netG(label, edges=edges)
+                else:
+                    generated = netEMA(label, edges=edges)  # [2, 3, 256, 256] [-1,1]
+
+                # SSIM
+                input1 = transform2(generated)
+                input2 = transform2(image)
+                ssim_value = pytorch_msssim.ssim(input1, input2)
+                ssim.append(ssim_value.mean().item())
+                ssim += [ssim_value]
+                # PIPS lpips
+                d = loss_fn_alex(input1, input2)
+                #feature1 = pips_model(input1)
+                #feature2 = pips_model(input2)
+                #pips_val = torch.cosine_similarity(feature1, feature2, dim=1)
+                pips.append(d.mean().item())
+                #pips += [pips_val.mean()]
+                # PSNR, RMSE
+                input1 = transform1(generated)
+                input2 = transform1(image)
+                mse = torch.nn.functional.mse_loss(input1, input2)
+                max_pixel_value = 1.0
+                psnr_value = 10 * torch.log10((max_pixel_value ** 2) / mse)
+                rmse_value = torch.sqrt(mse)
+                psnr.append(psnr_value.mean().item())
+                rmse.append(rmse_value.mean().item())
+
+        netG.train()
+        if not self.opt.no_EMA:
+            netEMA.train()
+
+        avg_pips = sum(pips) / total_samples
+        avg_ssim = sum(ssim) / total_samples
+        avg_psnr = sum(psnr) / total_samples
+        avg_rmse = sum(rmse) / total_samples
+        avg_pips = np.array(avg_pips)
+        avg_ssim = np.array(avg_ssim)
+        avg_psnr = np.array(avg_psnr)
+        avg_rmse = np.array(avg_rmse)
+
+        return avg_pips, avg_ssim, avg_psnr, avg_rmse
+
+
+
+    def update_metrics(self, model, cur_iter):
+        print("--- Iter %s: computing PIPS SSIM PSNR RMSE---" % (cur_iter))
+        cur_pips, cur_ssim, cur_psnr, cur_rmse= self.compute_metrics(model.module.netG, model.module.netEMA, model)
+        self.update_logs(cur_pips, cur_iter, 'PIPS')
+        self.update_logs(cur_ssim, cur_iter, 'SSIM')
+        self.update_logs(cur_psnr, cur_iter, 'PSNR')
+        self.update_logs(cur_rmse, cur_iter, 'RMSE')
+
+        print("--- Metrics at Iter %s: " % cur_iter, "{:.2f}".format(cur_pips),"{:.2f}".format(cur_ssim),"{:.2f}".format(cur_psnr),"{:.2f}".format(cur_rmse))
+
+
+    def update_logs(self, cur_data, epoch, mode):
+        try:
+            np_file = np.load(os.path.join(self.path_to_save, mode, f"{mode}_log.npy"), allow_pickle=True)
+            first = list(np_file[0, :])
+            sercon = list(np_file[1, :])
+            first.append(epoch)
+            sercon.append(cur_data)
+            np_file = [first, sercon]
+        except:
+            np_file = [[epoch], [cur_data]]
+
+        np.save(os.path.join(self.path_to_save, mode, f"{mode}_log.npy"), np_file)
+        np_file = np.array(np_file)
+        plt.figure()
+        plt.plot(np_file[0, :], np_file[1, :])
+        plt.grid(b=True, which='major', color='#666666', linestyle='--')
+        plt.minorticks_on()
+        plt.grid(b=True, which='minor', color='#999999', linestyle='--', alpha=0.2)
+        plt.savefig(os.path.join(self.path_to_save, mode, f"plot_{mode}"), dpi=600)
+        plt.close()
