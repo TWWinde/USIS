542f1be3cec281d4115943ec362ad9a55dd48ced
TWWinde <twwinde@gmail.com>
2023-12-17 22:03:11 +0100

    Update usis.sh

--- usis.sh
+++ usis.sh
@@ -11,7 +11,7 @@
 #SBATCH --gpus=1
 #SBATCH --qos=batch
 # SBATCH --nodes=1
-#SBATCH --gpus=rtx_a5000:1
+# SBATCH --gpus=rtx_a5000:1
 # SBATCH --gpus=geforce_rtx_2080ti:1
 # SBATCH --gpus=geforce_gtx_titan_x:1
 
@@ -29,17 +29,17 @@ module load cuda
 #--model_supervision 0 --netG wavelet --channels_G 16  #16
 
 #experiment_2
-python train.py --name usis_wavelet_no_mask --dataset_mode ct2mri --gpu_ids 0 \
---dataroot /misc/data/private/autoPET/CT_MR --batch_size 4 \
---netDu wavelet --continue_train \
---model_supervision 0 --netG wavelet --channels_G 16  #16
+#python train.py --name usis_wavelet_no_mask --dataset_mode ct2mri --gpu_ids 0 \
+#--dataroot /misc/data/private/autoPET/CT_MR --batch_size 4 \
+#--netDu wavelet --continue_train \
+#--model_supervision 0 --netG wavelet --channels_G 16  #16
 
 
 #experiment_3.
-#python train.py --name usis_oasis_generator --dataset_mode ct2mri --gpu_ids 0 \
-#--dataroot /misc/data/private/autoPET/CT_MR --batch_size 2 --add_mask  \
-#--netDu wavelet --continue_train \
-#--model_supervision 0 --netG oasis --channels_G 64
+python train.py --name usis_oasis_generator --dataset_mode ct2mri --gpu_ids 0 \
+--dataroot /misc/data/private/autoPET/CT_MR --batch_size 2 --add_mask  \
+--netDu wavelet --continue_train \
+--model_supervision 0 --netG oasis --channels_G 64
 
 #experiment_4
 #python train.py --name usis_oasis_generator_no_mask --dataset_mode ct2mri --gpu_ids 0 \
