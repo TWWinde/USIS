177b4b7fe1c2d22b206c14ac27d0ab24881578c4
TWWinde <twwinde@gmail.com>
2023-12-17 17:53:17 +0100

    Update usis.sh

--- usis.sh
+++ usis.sh
@@ -11,7 +11,7 @@
 #SBATCH --gpus=1
 #SBATCH --qos=batch
 # SBATCH --nodes=1
-#SBATCH --gpus=rtx_a5000:1
+# SBATCH --gpus=rtx_a5000:1
 # SBATCH --gpus=geforce_rtx_2080ti:1
 # SBATCH --gpus=geforce_gtx_titan_x:1
 
@@ -23,16 +23,16 @@ module load cuda
 # Run your python code
 
 #experiment_1
-#python train.py --name usis_wavelet --dataset_mode ct2mri --gpu_ids 0 \
-#--dataroot /misc/data/private/autoPET/CT_MR --batch_size 2 --add_mask  \
-#--netDu wavelet --continue_train \
-#--model_supervision 0 --netG wavelet --channels_G 16  #16
-
-#experiment_2
-python train.py --name usis_wavelet_no_mask --dataset_mode ct2mri --gpu_ids 0 \
---dataroot /misc/data/private/autoPET/CT_MR --batch_size 4 \
+python train.py --name usis_wavelet --dataset_mode ct2mri --gpu_ids 0 \
+--dataroot /misc/data/private/autoPET/CT_MR --batch_size 2 --add_mask  \
 --netDu wavelet --continue_train \
 --model_supervision 0 --netG wavelet --channels_G 16  #16
+
+#experiment_2
+#python train.py --name usis_wavelet_no_mask --dataset_mode ct2mri --gpu_ids 0 \
+#--dataroot /misc/data/private/autoPET/CT_MR --batch_size 4 \
+#--netDu wavelet --continue_train \
+#--model_supervision 0 --netG wavelet --channels_G 16  #16
 
 
 #experiment_3.
