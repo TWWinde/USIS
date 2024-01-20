3c894e42d6a9c4acfbb36bcc103d6c0c4c1229a3
TWWinde <twwinde@gmail.com>
2024-01-20 20:24:43 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -24,9 +24,9 @@ module load cuda
 
 #test
 #experiment_1
-#python test.py --name usis_wavelet --dataset_mode ct2mri --gpu_ids 0 \
-#--dataroot /misc/data/private/autoPET/CT_MR --batch_size 20 --model_supervision 0 \
-#--model_supervision 0 --netG wavelet --channels_G 16
+python test.py --name usis_wavelet --dataset_mode ct2mri --gpu_ids 0 \
+--dataroot /misc/data/private/autoPET/CT_MR --batch_size 20 --model_supervision 0 \
+--model_supervision 0 --netG wavelet --channels_G 16
 
 #experiment_2
 #python test.py --name usis_wavelet_no_mask --dataset_mode ct2mri --gpu_ids 0 \
@@ -40,6 +40,6 @@ module load cuda
 #--model_supervision 0 --netG oasis --channels_G 64
 
 #experiment_4
-python test.py --name usis_oasis_generator_no_mask --dataset_mode ct2mri --gpu_ids 0 \
---dataroot /misc/data/private/autoPET/CT_MR --batch_size 20 --model_supervision 0 \
---model_supervision 0 --netG oasis --channels_G 64
+#python test.py --name usis_oasis_generator_no_mask --dataset_mode ct2mri --gpu_ids 0 \
+#--dataroot /misc/data/private/autoPET/CT_MR --batch_size 20 --model_supervision 0 \
+#--model_supervision 0 --netG oasis --channels_G 64
