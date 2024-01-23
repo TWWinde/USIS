a515a855266fc855e99d7913a229c45e667b77ff
TWWinde <twwinde@gmail.com>
2024-01-23 18:00:45 +0100

    Update usis.sh

--- usis.sh
+++ usis.sh
@@ -47,14 +47,14 @@ module load cuda
 #--netDu wavelet  --continue_train \
 #--model_supervision 0 --netG oasis --channels_G 64
 
-#experiment_5 unpaired ct autopet
-python train.py --name unpaired_ct_autopet --dataset_mode ct2ctautopet --gpu_ids 0 \
---dataroot /misc/data/private/autoPET/data_nnunet --batch_size 4  \
---netDu wavelet  --continue_train \
---model_supervision 0 --netG wavelet --channels_G 16
+#experiment_5 unpaired ct autopet 117714
+#python train.py --name unpaired_ct_autopet --dataset_mode ct2ctautopet --gpu_ids 0 \
+#--dataroot /misc/data/private/autoPET/data_nnunet --batch_size 4  \
+#--netDu wavelet  --continue_train \
+#--model_supervision 0 --netG wavelet --channels_G 16
 
 #experiment_6 no_3d_noise
-#python train.py --name usis_wavelet --dataset_mode ct2mri --gpu_ids 0 \
+#python train.py --name usis_wavelet_no3dnoise --dataset_mode ct2mri --gpu_ids 0 \
 #--dataroot /misc/data/private/autoPET/CT_MR --batch_size 4 --add_mask  \
 #--netDu wavelet --continue_train --z_dim 0 --no_3dnoise --continue_train \
 #--model_supervision 0 --netG wavelet --channels_G 16  #16
