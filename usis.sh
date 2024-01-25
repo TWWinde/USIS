f637b83bc355aceb4f69e8ebaf86e574fb6a6a60
TWWinde <twwinde@gmail.com>
2024-01-25 19:30:40 +0100

    Update usis.sh

--- usis.sh
+++ usis.sh
@@ -23,10 +23,10 @@ module load cuda
 # Run your python code
 
 #experiment_1 117718
-python train.py --name usis_wavelet --dataset_mode ct2mri --gpu_ids 0 \
---dataroot /misc/data/private/autoPET/CT_MR --batch_size 4 --add_mask  \
---netDu wavelet --continue_train \
---model_supervision 0 --netG wavelet --channels_G 16  #16
+#python train.py --name usis_wavelet --dataset_mode ct2mri --gpu_ids 0 \
+#--dataroot /misc/data/private/autoPET/CT_MR --batch_size 4 --add_mask  \
+#--netDu wavelet --continue_train \
+#--model_supervision 0 --netG wavelet --channels_G 16  #16
 
 #experiment_2
 #python train.py --name usis_wavelet_no_mask --dataset_mode ct2mri --gpu_ids 0 \
@@ -48,10 +48,10 @@ python train.py --name usis_wavelet --dataset_mode ct2mri --gpu_ids 0 \
 #--model_supervision 0 --netG oasis --channels_G 64
 
 #experiment_5 unpaired ct autopet 117714
-#python train.py --name unpaired_ct_autopet --dataset_mode ct2ctautopet --gpu_ids 0 \
-#--dataroot /misc/data/private/autoPET/data_nnunet --batch_size 4  \
-#--netDu wavelet  --continue_train \
-#--model_supervision 0 --netG wavelet --channels_G 16
+python train.py --name unpaired_ct_autopet --dataset_mode ct2ctautopet --gpu_ids 0 \
+--dataroot /misc/data/private/autoPET/data_nnunet --batch_size 4  \
+--netDu wavelet  --continue_train \
+--model_supervision 0 --netG wavelet --channels_G 16
 
 #experiment_6 no_3d_noise 117717
 #python train.py --name usis_wavelet_one2one --dataset_mode ct2mri --gpu_ids 0 \
