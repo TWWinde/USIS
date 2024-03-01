bff156a1260eb37bdb7c357fbb2767a452f4cf06
TWWinde <twwinde@gmail.com>
2024-03-01 10:27:43 +0100

    Update usis.sh

--- usis.sh
+++ usis.sh
@@ -62,13 +62,13 @@ module load cuda
 #experiment_7
 #python train.py --name wavelet_no_mask --dataset_mode ct2mri --gpu_ids 0 \
 #--dataroot /misc/data/private/autoPET/CT_MR --batch_size 4 \
-#--netDu wavelet --continue_train \
+#--netDu wavelet  \
 #--model_supervision 0 --netG wavelet --channels_G 16  #16
 
 #experiment_8
 python train.py --name wavelet_with_mask --dataset_mode ct2mri --gpu_ids 0 \
 --dataroot /misc/data/private/autoPET/CT_MR --batch_size 4 --add_mask  \
---netDu wavelet --continue_train \
+--netDu wavelet  \
 --model_supervision 0 --netG wavelet --channels_G 16  #16
 
 #experiment_5 unpaired ct autopet 117714
