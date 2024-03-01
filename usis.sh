c83cd1e63033d20d46e608b34fbb92f48b75b5f0
TWWinde <twwinde@gmail.com>
2024-03-01 10:26:18 +0100

    Update usis.sh

--- usis.sh
+++ usis.sh
@@ -60,16 +60,16 @@ module load cuda
 #--model_supervision 0 --netG wavelet --channels_G 16  #16
 
 #experiment_7
-python train.py --name wavelet_no_mask --dataset_mode ct2mri --gpu_ids 0 \
---dataroot /misc/data/private/autoPET/CT_MR --batch_size 4 \
---netDu wavelet --continue_train \
---model_supervision 0 --netG wavelet --channels_G 16  #16
-
-#experiment_8
-#python train.py --name wavelet_with_mask --dataset_mode ct2mri --gpu_ids 0 \
-#--dataroot /misc/data/private/autoPET/CT_MR --batch_size 4 --add_mask  \
+#python train.py --name wavelet_no_mask --dataset_mode ct2mri --gpu_ids 0 \
+#--dataroot /misc/data/private/autoPET/CT_MR --batch_size 4 \
 #--netDu wavelet --continue_train \
 #--model_supervision 0 --netG wavelet --channels_G 16  #16
+
+#experiment_8
+python train.py --name wavelet_with_mask --dataset_mode ct2mri --gpu_ids 0 \
+--dataroot /misc/data/private/autoPET/CT_MR --batch_size 4 --add_mask  \
+--netDu wavelet --continue_train \
+--model_supervision 0 --netG wavelet --channels_G 16  #16
 
 #experiment_5 unpaired ct autopet 117714
 #python train.py --name unpaired_ct_mask --dataset_mode ct2ctautopet --gpu_ids 0 \
